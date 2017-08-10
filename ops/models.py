6372ca29d06c4d541c2fc31070e14872422366a6
Live session user <1@1.1>
2017-08-10 18:51:22 +0000

    remove stale code comments

--- ops/models.py
+++ ops/models.py
@@ -28,28 +28,3 @@ class Cmds(models.Model):
     cmdset = models.CharField(max_length=255, default='')
     cmdline = models.CharField(max_length=255, default='\n')
     timestamp = models.DateTimeField(auto_now_add=True)
-
-"""
-class CmdSystems(models.Model):
-    CMDSYSTEMS_CHOICES = (
-        (1,'AMS5520'),
-        (2,'AXSVision'),
-        (3,'Calix'),
-        (4,'JunOS'),
-        (5,'JunOSe'),
-        (6,'Triad'),
-    )
-    cmdprotocolkey = models.ForeignKey('CmdProtocols', on_delete=models.CASCADE)
-    cmdsystem = models.CharField(max_length=255, choices=CMDSYSTEMS_CHOICES)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-class CmdSets(models.Model):
-    cmdsystemkey = models.ForeignKey('CmdSystems', on_delete=models.CASCADE)
-    cmdset = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-class Cmds(models.Model):
-    cmdsetkey = models.ForeignKey('CmdSystems', on_delete=models.CASCADE)
-    cmdset = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
-"""
--- ops/serializers.py
+++ ops/serializers.py
@@ -17,24 +17,3 @@ class CmdSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cmds
         fields = ('url', 'cmdprotocol', 'cmdsystem', 'cmdset', 'cmdline', 'timestamp')
-
-"""
-class CmdSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    cmdprotocol = serializers.ChoiceField(choices=CMDPROTOCOL_CHOICES, default=0)
-    cmdsystem = serializers.ChoiceField(choices=CMDSYSTEM_CHOICES, default=0)
-    cmdset = serializers.CharField(max_length=255, default='')
-    cmdline = serializers.CharField(max_length=255, default='\n')
-    timestamp = serializers.DateTimeField(read_only=True, default='')
-
-    def create(self, validated_date):
-        return ops.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.cmdprotocol = validated_data.get('cmdprotocol', instance.cmdprotocol)
-        instance.cmdsystem = validated_data.get('cmdsystem', instance.cmdsystem)
-        instance.cmdset = validated_data.get('cmdset', instance.cmdset)
-        instance.cmdline = validated_data.get('cmdline', instance.cmdline)
-        instance.save()
-        return instance
-"""
